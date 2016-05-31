1d5d90d89138aef7279221e89a83788a1b6d5b4f
Yogeshwar Dan Charan <charanyogeshwar@gmail.com>
2016-05-31 23:28:00 +0530

    feat : Add timestamp.

--- app/src/main/java/digimpact/org/rsattendance/activity/AttendanceActivity.java
+++ app/src/main/java/digimpact/org/rsattendance/activity/AttendanceActivity.java
@@ -4,21 +4,52 @@ import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.telephony.TelephonyManager;
+import android.text.format.DateFormat;
 import android.util.Log;
+import android.widget.TextView;
 import android.widget.Toast;
+
+
+
+import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
+import java.util.TimeZone;
 
 import digimpact.org.rsattendance.R;
 
 public class AttendanceActivity extends AppCompatActivity {
+
+    private TextView mImeiTextView;
+    private TextView mTimeStampView ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.attendance_activity);
 
+        mImeiTextView = (TextView)findViewById(R.id.imei_text_view);
+        mTimeStampView = (TextView)findViewById(R.id.time_stamp_text_view);
+
+
         TelephonyManager telephonyManager = (TelephonyManager)getSystemService(Context.TELEPHONY_SERVICE);
         String id = telephonyManager.getDeviceId();
-        Toast.makeText(AttendanceActivity.this, id, Toast.LENGTH_LONG).show();
+        mImeiTextView.setText(id);
+
         Log.d("Attendance",id);
+
+        Long time = System.currentTimeMillis()/1000;
+
+        mTimeStampView.setText(time.toString());
+
+
+
+
+
+
+
+        ;
     }
 }
